ce2addcbdedb0c80fdc7d3681cbb1a93d9ea91c7
Михаил Петров <mikhailpetrov80@gmail.com>
2021-11-10 15:59:40 +0400

    Изменения 10.11.21_2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int SIZE = scanner.nextInt();
+        System.out.println();
+
+        scanner.nextLine();
+        String line = scanner.nextLine();
+        String[] time = line.split(",");
+        int[] array = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = Integer.parseInt(time[i]);
+        }
+
+        System.out.println();
+
+        Arrays.sort(array);
+
+        int count = 0;
+        for (int i = 0; i < SIZE - 1; i++) {
+            if (array[i] == array[i + 1]) {
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
 }
+
